b6d3935a489055f664a22457e498ae4e32f72e54
dennis <dennis19876@googlemail.com>
2024-10-28 13:37:36 +0100

    remote origin geändert

--- task-1.js
+++ task-1.js
@@ -1,12 +1,28 @@
 // Target array
-const people = []
+const people = [];
 
 // Source data
-const firstNames = ["Mark", "Sandra", "Dina", "Mikoel"]
-const lastNames = ["Shark", "Mellow", "Saur", "Boomex"]
-const ages = [18, 27, 31, 44]
-const cities = ["New York", "Berlin", "Barcelona", "Windhoek"]
+const firstNames = ["Mark", "Sandra", "Dina", "Mikoel"];
+const lastNames = ["Shark", "Mellow", "Saur", "Boomex"];
+const ages = [18, 27, 31, 44];
+const cities = ["New York", "Berlin", "Barcelona", "Windhoek"];
 
 // Your code here!
 
-console.log(people)
+for (let i = 0; i < firstNames.length; i++) {
+  const person = {
+    firstName: firstNames[i],
+    lastName: lastNames[i],
+    age: ages[i],
+    city: cities[i],
+    fullName: `${firstNames[i]} ${lastNames[i]}`,
+
+    introduce: function () {
+      return `Hello, my name is ${this.fullName}, I live in ${this.city} and i Am ${this.age} years old`;
+    },
+  };
+  people.push(person);
+}
+
+console.log(people);
+console.log(people[0].introduce());
