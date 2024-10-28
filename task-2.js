9b026b9220392eb72f0c4a1ff4f0fe03d7897981
dennis <dennis19876@googlemail.com>
2024-10-28 13:59:37 +0100

    done

--- task-2.js
+++ task-2.js
@@ -1,10 +1,15 @@
 const person = {
   firstName: "Tim",
   age: 28,
-}
+
+  greet: function (greeting) {
+    const capitalizedGreeting = greeting[0].toUpperCase() + greeting.slice(1);
+    return `${capitalizedGreeting}, my name is ${this.firstName}`;
+  },
+};
 
 // Your code here!
 
-console.log(person.greet("hi")) // Hi, my name is Tim
-console.log(person.greet("hey")) // Hey, my name is Tim
-console.log(person.greet("hello")) // Hello, my name is Tim
+console.log(person.greet("hi")); // Hi, my name is Tim
+console.log(person.greet("hey")); // Hey, my name is Tim
+console.log(person.greet("hello")); // Hello, my name is Tim
--- task-3.js
+++ task-3.js
@@ -1,1 +1,43 @@
 // Your code here!
+
+const player1 = {
+  name: "Ryu",
+  health: 50,
+  attackSkill: "punch",
+  attackDamage: 16,
+
+  attack: function (opponent) {
+    opponent.health -= this.attackDamage;
+    if (opponent.health < 0) opponent.health = 0; // Ensure health doesn't go negative
+    console.log(
+      `${this.name} attacked ${opponent.name} with a ${this.attackSkill}, inflicting ${this.attackDamage} damage. ${opponent.name}'s health is now ${opponent.health}.`
+    );
+
+    if (opponent.health === 0) {
+      console.log(`${opponent.name} has been defeated!`);
+    }
+  },
+};
+
+const player2 = {
+  name: "Ken",
+  health: 40,
+  attackSkill: "flying kick",
+  attackDamage: 24,
+
+  attack: function (opponent) {
+    opponent.health -= this.attackDamage;
+    if (opponent.health < 0) opponent.health = 0; // Ensure health doesn't go negative
+    console.log(
+      `${this.name} attacked ${opponent.name} with a ${this.attackSkill}, inflicting ${this.attackDamage} damage. ${opponent.name}'s health is now ${opponent.health}.`
+    );
+
+    if (opponent.health === 0) {
+      console.log(`${opponent.name} has been defeated!`);
+    }
+  },
+};
+
+// Test the attack functionality
+player1.attack(player2); // Ryu attacked Ken with a punch, inflicting 16 damage. Ken's health is now 24.
+player2.attack(player1); // Ken attacked Ryu with a flying kick, inflicting 24 damage. Ryu's health is now 26.
